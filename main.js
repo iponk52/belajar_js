3a30eadcb1ecc1ce50e59ac3052957cc3a8546a9
iponk52 <irfan.dharmawan.52@gmail.com>
2020-06-15 16:19:43 +0700

    belajar variable

---
+++ main.js
@@ -0,0 +1,6 @@
+/* Belajar var,lets,const*/
+let a = 5 //let membuat isi variable dapat di ubah
+const b = 5 //const membuat isi variable tetap
+var c = 5 //membuat isi variable dengan di baca pertama
+
+console.log(a) 
